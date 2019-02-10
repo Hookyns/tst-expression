f12f2af581b0c3dd5ea1bac8c4aff471136c1653
Hookyns <ncm.Hawkyns@seznam.cz>
2019-02-10 23:35:29 +0100

    Clearing code

--- demo/README.md
+++ demo/README.md
@@ -1,8 +1,8 @@
 # Expression Transformer DEMO
 Here is demo in this directory which shows one example usage of expression transformer. 
 Use `node compile.js` to compile example from TS to JS.
-Use `node output.js` to run example. 
+Use `node src/index.js` to run example. 
 
 ## UniMapperJS
 Next example can be [UniMapperJS](https://github.com/Hookyns/unimapperjs) package which is going to be rewritten, 
-changing string expression parsing for this package.
+changing string expression parsing for this package (not committed yet).
--- demo/compile.js
+++ demo/compile.js
@@ -1,6 +1,4 @@
 const ts = require("typescript");
-const $fs = require("fs");
-const $path = require("path");
 const glob = require("glob");
 const {parseExpressions} = require("../expression-transformer");
 
@@ -10,26 +8,6 @@ const options = require("./tsconfig");
 // options.include must be set; **/*.ts otherwise
 if (!options.hasOwnProperty("include")) {
 	options.include = ["./**/*.ts"];
-}
-
-/**
- * Read file content and append it to "files" object parameter
- * @param path
- * @param files
- * @returns {Promise<void>}
- */
-function readFileContent(path, files) {
-	return new Promise((resolve, reject) => {
-		$fs.readFile(path, "utf-8", (err, content) => {
-			if (err) {
-				reject(err);
-				return;
-			}
-
-			files.push({path, content});
-			resolve();
-		});
-	});
 }
 
 /**
@@ -48,9 +26,6 @@ async function getPathsFromPatterns(pattern, files) {
 					if (files.indexOf(path) === -1) {
 						files.push(path);
 					}
-					// promises.push(
-					// 	readFileContent(path, files)
-					// );
 				}
 				await Promise.all(promises);
 				resolve();
@@ -89,53 +64,8 @@ async function getFiles(options) {
 		const program = ts.createProgram(files, options);
 		program.emit(undefined, undefined, undefined, false, {before: [parseExpressions(program)]});
 
-		// for (let file of files) {
-		// 	if (file.path.slice(-5) !== ".d.ts") {
-		// 		// Name of output file
-		// 		let outFileName = $path.basename(file.path);
-		// 		outFileName = outFileName.slice(0, outFileName.length - $path.extname(file.path).length) + ".js";
-		// 		let outFilePath = $path.join($path.dirname(file.path), outFileName);
-		//
-		// 		let result = ts.transpileModule(file.content, {
-		// 			compilerOptions: options,
-		// 			transformers: {before: [parseExpressions(program)]}
-		// 		});
-		//
-		// 		promises.push(
-		// 			new Promise((resolve, reject) => {
-		// 				$fs.writeFile(outFilePath, result.outputText, "utf-8", err => {
-		// 					if (err) {
-		// 						reject(err);
-		// 						return;
-		// 					}
-		//
-		// 					resolve();
-		// 				});
-		// 			})
-		// 		);
-		// 	}
-		// }
-
 		await Promise.all(promises);
 	} catch (e) {
 		console.error(e);
 	}
 })();
-
-// const source = $fs.readFileSync(__dirname + "/code.ts", "utf-8");
-//
-// // const options = {
-// // 	module: ts.ModuleKind.CommonJS,
-// // 	target: "esnext",
-// // 	sourceMap: true,
-// // };
-//
-//
-// let result = ts.transpileModule(source, {
-// 	compilerOptions: options,
-// 	transformers: {before: [parseExpressions(program)]}
-// });
-//
-// // console.log(result);
-//
-// $fs.writeFileSync(__dirname + "/output.js", result.outputText, "utf-8");
