de45e686b5172339f5728f60258b8f2dd710fd40
Hookyns <hookcze@gmail.com>
2021-10-16 19:34:34 +0200

    dev/usage example

--- dev/usage/index.ts
+++ dev/usage/index.ts
@@ -3,7 +3,11 @@ import {
     assertExpression,
     Expression,
     ExpressionOnly
-} from "tst-expression";
+}                       from "tst-expression";
+import {
+    ImportedClass,
+    importedProp
+} from "./some-module";
 
 function logExpression(expression: ExpressionOnly<any>)
 {
@@ -23,4 +27,17 @@ function logParameters(expression: Expression<(...args: any[]) => any>)
     }
 }
 
-logParameters((x: string, y: number, z: { foo: any }) => z.foo && (x > z.foo || y < z.foo));
+const constProp = 1;
+let letProp = 2;
+const access = { propAccess: 3 };
+class Class {
+    static staticProp = 4;
+}
+
+
+logParameters(
+    (x: string, y: number, z: { foo: any, bar: (baz: string) => { baz: string } }) => 
+        z.foo && (x > z.foo || y < z.foo) && y < constProp && y < letProp && y < access.propAccess && y < Class.staticProp
+        && y < importedProp && y < ImportedClass.staticProp
+        && z.bar("bar").baz
+);
---
+++ dev/usage/some-module.ts
@@ -0,0 +1,4 @@
+export const importedProp = 1;
+export class ImportedClass {
+    static staticProp = 2;
+}
